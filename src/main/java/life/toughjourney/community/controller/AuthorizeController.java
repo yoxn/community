97f5e89385f849f0194d6e7900b9f5b5e5eef9c7
yoxn <yangxin_5@163.com>
2020-01-26 09:40:47 +0800

    extract guthub config from code to properties

---
+++ src/main/java/life/toughjourney/community/controller/AuthorizeController.java
@@ -0,0 +1,46 @@
+package life.toughjourney.community.controller;
+
+import life.toughjourney.community.dto.AccessTokenDto;
+import life.toughjourney.community.dto.GithubUser;
+import life.toughjourney.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author YX
+ * @version 1.0.0
+ * @Description
+ * @create 2020年01月25日 22:20:00
+ */
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.url}")
+    private String redirectUrl;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) {
+        AccessTokenDto accessTokenDto = new AccessTokenDto();
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSecret);
+        accessTokenDto.setCode(code);
+        accessTokenDto.setRedirect_url(redirectUrl);
+        accessTokenDto.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDto);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
---
+++ src/main/java/life/toughjourney/community/dto/GithubUser.java
@@ -0,0 +1,37 @@
+package life.toughjourney.community.dto;
+
+/**
+ * @author YX
+ * @version 1.0.0
+ * @Description
+ * @create 2020年01月25日 23:25:00
+ */
+public class GithubUser {
+    private String name;
+    private Long id;
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
